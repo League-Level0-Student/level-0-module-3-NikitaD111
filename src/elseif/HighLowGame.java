82a1cf89c824a9b87ac54847538f65b097952563
CatcusCat <league@iMac.attlocal.net>
2018-01-10 18:28:52 -0800

    HighLowGame,ShinyObjects,WalkOfFame

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,27 +5,43 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(101);
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. do the following 10 times
+		for (int i = 0; i < 10; i++) {
+			
 		
 			// 1. ask the user for a guess using a pop-up window, and save their response 
-
+String a = JOptionPane.showInputDialog("Guess");
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
+		int b =	Integer.parseInt(a);
 			// 5. if the guess is correct
 				// 6. win
+			if(b == random){
+JOptionPane.showMessageDialog(null,"you win");	
+
+			}
 			// 7. if the guess is high
 				// 8. tell them it's too high
+			if(b > random) {
+				JOptionPane.showMessageDialog(null, "To high");
+			}
 			// 9. if the guess is low
 				// 10. tell them it's too low
-
+			if(b < random) {
+				JOptionPane.showMessageDialog(null, "Thats too low");
+			}
+		}
 		// 12. tell them they lose
+		JOptionPane.showMessageDialog(null, "You lose get outta here");
 	}
 
 }
--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -9,20 +9,35 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		// 1. Set the X position of the robot so that it starts on the left. 
+		// 1. Set the X position of the robot so that it starts on the left.
 		// You also need to show the robot to see the result of this line.
-
+int x = 100;
+rob.setX(x);
+rob.setY(250);
+		rob.setSpeed(100);
 		// 2. Make the robot draw a star shape. Hint: 144.
+		for (int i = 0; i < 5; i++) {
+			for (int i1 = 0; i1 < 5; i1++) {
+				rob.penDown();
+				rob.move(70);
+				rob.turn(144);
+			}
+			rob.setX(x);
+			rob.setY(250);
 
+			}
+		
 		// 3. Set the size of the star to 30.
+		rob.setPenWidth(30);
 
 		/** THE CHALLENGE: **/
 		/*
-		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/walk-of-fame
+		 * Make the robot draw a line of stars like this: http://bit.ly/walk-of-fame
 		 * 
 		 * Hint: The distance between stars is 50.
+		 * 
 		 */
+		rob.setX(200);
 
 	}
 
